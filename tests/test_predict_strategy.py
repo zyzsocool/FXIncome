2725024eee803f0f18bd0e3670f1688318a74e00
Dominic <panda@vip.qq.com>
2023-09-22 16:22:38 +0800

    Test PredictionStrategy

--- tests/test_predict_strategy.py
+++ tests/test_predict_strategy.py
@@ -1,37 +1,44 @@
 from fxincome.backtest.spread_backtrader import SpreadData, PredictStrategy
+import datetime
 import backtrader as bt
 import pandas as pd
 import pytest
-from fxincome.const import PATH, SPREAD
+import matplotlib.pyplot as plt
 from fxincome import logger, f_logger
 
 
-def main():
-    leg1_code = "220210"
-    leg2_code = "220215"
-    cerebro = bt.Cerebro()
-    input_file = PATH.SPREAD_DATA + leg1_code + "_" + leg2_code + "_test_bt.csv"
-    price_df = pd.read_csv(input_file, parse_dates=["DATE"])
-    # minimum spread in the past 15 days are used as the threshold to open a position
-    price_df["SPREAD_MIN"] = price_df["SPREAD"].rolling(15).min()
-    price_df.loc[:, ["SPREAD_MIN"]] = price_df.loc[:, ["SPREAD_MIN"]].fillna(
-        method="backfill"
-    )
-    price_df = price_df.dropna()
-    # data_long and data_short are the same data feeds, but with different names.
-    # data_long is for predicting long strategy, and data_short is for predicting short strategy.
-    data_long = SpreadData(dataname=price_df, nocase=True)
-    cerebro.adddata(data_long, name=PredictStrategy.ST_NAME)
-    cerebro.addstrategy(PredictStrategy)
-    cerebro.broker.set_cash(PredictStrategy.INIT_CASH)
-    logger.info(leg1_code + "_" + leg2_code)
-    strategies = cerebro.run()
-    profit = (cerebro.broker.get_value() - PredictStrategy.INIT_CASH) / 10000
-    logger.info(
-        f"PROFIT: {(cerebro.broker.get_value() - PredictStrategy.INIT_CASH) / 10000:.2f}"
-    )
-    assert profit == pytest.approx(-1.69, abs=0.01)
+class TestSpreadPortfolio:
+    @pytest.fixture(scope="class")
+    def global_data(self):
+        global_data = {}
+        global_data["leg1_code"] = "220210"
+        global_data["leg2_code"] = "220215"
+        global_data[
+            "input_file"
+        ] = f'./test_data/{global_data["leg1_code"]}_{global_data["leg2_code"]}_test_bt.csv'
+        global_data["price_df"] = pd.read_csv(
+            global_data["input_file"], parse_dates=["DATE"]
+        )
+        # minimum spread in the past 15 days are used as the threshold to open a position
+        global_data["price_df"]["SPREAD_MIN"] = (
+            global_data["price_df"]["SPREAD"].rolling(15).min()
+        )
+        global_data["price_df"].loc[:, ["SPREAD_MIN"]] = (
+            global_data["price_df"].loc[:, ["SPREAD_MIN"]].fillna(method="backfill")
+        )
+        global_data["price_df"] = global_data["price_df"].dropna()
+        return global_data
 
-
-if __name__ == "__main__":
-    main()
+    def test_profit(self, global_data):
+        cerebro = bt.Cerebro()
+        data_long = SpreadData(dataname=global_data["price_df"], nocase=True)
+        cerebro.adddata(data_long, name=PredictStrategy.ST_NAME)
+        cerebro.addstrategy(PredictStrategy)
+        cerebro.broker.set_cash(PredictStrategy.INIT_CASH)
+        logger.info(global_data["leg1_code"] + "_" + global_data["leg2_code"])
+        strategies = cerebro.run()
+        profit = (cerebro.broker.get_value() - PredictStrategy.INIT_CASH) / 10000
+        logger.info(
+            f"PROFIT: {(cerebro.broker.get_value() - PredictStrategy.INIT_CASH) / 10000:.2f}"
+        )
+        assert profit == pytest.approx(-1.69, abs=0.01)
