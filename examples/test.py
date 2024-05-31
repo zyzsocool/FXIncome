0316ebba58ce3ab2d504fcf8152f263772e0d6e7
Dominic <panda@vip.qq.com>
2024-05-31 17:59:58 +0800

    Process data for history similarity strategy.

--- examples/test.py
+++ examples/test.py
@@ -1,30 +1,10 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
-import datetime
+import os
 
-from fxincome.const import COUPON_TYPE
-from fxincome.const import CASHFLOW_TYPE
-from dateutil.relativedelta import relativedelta
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', None)
-from fxincome.asset import Bond
-from fxincome.position import Position_Bond
-from fxincome.portfolio import Portfolio_Bond
-from fxincome.reading import excel_to_portfolio_bond
+ROOT_PATH = "d:/ProjectRicequant/fxincome/strategies_pool"
+SRC_NAME = "history_processed.csv"
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', None)
-
-
-address = r'./try.xlsx'
-portfolio_bond=excel_to_portfolio_bond(address)
-# print(portfolio_bond.get_cashflow())
-# print(portfolio_bond.get_cashflow('Agg'))
-# print(portfolio_bond.get_position_gain('Raw'))
-# print(portfolio_bond.get_position_gain('Agg'))
-# print(portfolio_bond.get_view())
-portfolio_bond.get_plot()
-result = portfolio_bond.get_view()
-result.to_csv(f"./result-{datetime.datetime.now().strftime('%Y%m%d-%H%M')}.csv", index=False, encoding='utf-8')
+data = pd.read_csv(os.path.join(ROOT_PATH, SRC_NAME), parse_dates=["date"])
+# Assuming 'df' is your DataFrame and 'date' is the column with the dates
+trading_days_per_year = data.resample('Y', on='date').size()
+print(trading_days_per_year)
